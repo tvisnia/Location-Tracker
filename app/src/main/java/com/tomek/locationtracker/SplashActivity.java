bbc05770e81fbcb23bedbc0219217ad97044840d
Tomek NA <masterworks41@gmail.com>
2016-07-09 17:17:51 +0200

    +splash screen delay 2000ms +manifest fixed +retrofit +ext +gitignore fixed

--- app/src/main/java/com/tomek/locationtracker/SplashActivity.java
+++ app/src/main/java/com/tomek/locationtracker/SplashActivity.java
@@ -2,6 +2,7 @@ package com.tomek.locationtracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,12 +10,20 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private static final long SPLASH_DISPLAY_LENGTH = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
+
 }
