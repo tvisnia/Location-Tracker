0952e8012c701fe5ffb0c727e8fe52d84b2628ca
Tomek NA <masterworks41@gmail.com>
2016-07-09 19:34:08 +0200

    +SplashActivity fixed

--- app/src/main/java/com/tomek/locationtracker/SplashActivity.java
+++ app/src/main/java/com/tomek/locationtracker/SplashActivity.java
@@ -1,14 +1,10 @@
 package com.tomek.locationtracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-import java.lang.ref.WeakReference;
 
 /**
  * Created by tomek on 08.07.16.
@@ -16,46 +12,26 @@ import java.lang.ref.WeakReference;
 public class SplashActivity extends AppCompatActivity {
 
     private static final long SPLASH_DISPLAY_LENGTH = 2000;
-    private static Context context;
-
-    private static class MyHandler extends Handler {
-        private final WeakReference<SplashActivity> mActivity;
-
-        public MyHandler(SplashActivity activity) {
-            mActivity = new WeakReference<SplashActivity>(activity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            SplashActivity activity = mActivity.get();
-            if (activity != null) {
-                Log.d(activity.getClass().getSimpleName(), activity.getString(R.string.on_handled));
-            }
-        }
-    }
-
-    private final MyHandler mHandler = new MyHandler(SplashActivity.this);
-    private static final Runnable activityDelayRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Intent intent = new Intent(context, MainActivity.class);
-            context.startActivity(intent);
-
-        }
-    };
+    private static final String TAG_HANDLER = "LaunchDelayHandler : ";
+    private static final java.lang.String TAG_ONSTOP = "onStopped";
+    private Handler launchScreenDelayHandler;
+    private Runnable activityDelayRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = this;
-        mHandler.postDelayed(activityDelayRunnable, SPLASH_DISPLAY_LENGTH);
+        activityDelayRunnable = () -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        };
+        launchScreenDelayHandler = new Handler();
+        launchScreenDelayHandler.postDelayed(activityDelayRunnable, SPLASH_DISPLAY_LENGTH);
     }
 
     @Override
     protected void onStop() {
-        Log.d(SplashActivity.this.getClass().getSimpleName(), getString(R.string.onStop));
-
-        mHandler.removeCallbacks(activityDelayRunnable);
+        Log.d(TAG_HANDLER, TAG_ONSTOP);
+        launchScreenDelayHandler.removeCallbacks(activityDelayRunnable);
         finish();
         super.onStop();
     }
