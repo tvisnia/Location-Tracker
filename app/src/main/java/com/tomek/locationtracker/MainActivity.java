4c2ff8af59928155bb0ba8f147774da3925ae85a
Tomek NA <masterworks41@gmail.com>
2016-07-10 21:27:45 +0200

    +requesting localization state

--- app/src/main/java/com/tomek/locationtracker/MainActivity.java
+++ app/src/main/java/com/tomek/locationtracker/MainActivity.java
@@ -1,13 +1,16 @@
 package com.tomek.locationtracker;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        checkLocationServices();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,5 +50,50 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         moveTaskToBack(true);
         finish();
+    }
+
+    private boolean isLocationEnabled() {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        } else {
+            locationProviders = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    private void checkLocationServices() {
+        if (!isLocationEnabled()) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.dialog_title)
+                    .content(R.string.dialog_content)
+                    .positiveText(R.string.agree)
+                    .negativeText(R.string.disagree)
+                    .callback(new MaterialDialog.ButtonCallback() {
+                        @Override
+                        public void onPositive(MaterialDialog dialog) {
+                            Intent locationSettingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(locationSettingsIntent);
+                            super.onPositive(dialog);
+                        }
+
+                        @Override
+                        public void onNegative(MaterialDialog dialog) {
+                            finish();
+                            super.onNegative(dialog);
+                        }
+                    })
+                    .build()
+                    .show();
+        }
     }
 }
