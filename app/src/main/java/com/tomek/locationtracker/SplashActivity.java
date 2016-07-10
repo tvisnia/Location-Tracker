5e9ca52363dcedcd20353bbb3136c6dddc48cfe0
Tomek NA <masterworks41@gmail.com>
2016-07-10 12:22:15 +0200

    +SplashActivity fixed 3 +Runnable as a static inner class Handler nie może być inicjalizowany przy deklaracji w onCreate, bo inaczej nie usunie callbacków w onStopie

--- app/src/main/java/com/tomek/locationtracker/SplashActivity.java
+++ app/src/main/java/com/tomek/locationtracker/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.tomek.locationtracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by tomek on 08.07.16.
@@ -13,16 +16,28 @@ public class SplashActivity extends AppCompatActivity {
 
     private static final long SPLASH_DISPLAY_LENGTH = 2000;
     private Handler launchScreenDelayHandler;
-    private static Runnable activityDelayRunnable;
+    private ActivityDelay activityDelayRunnable;
+    private static class ActivityDelay implements Runnable {
+        final WeakReference<Context> ctxReference;
+
+        public ActivityDelay(SplashActivity activity) {
+            ctxReference = new WeakReference<>(activity.getApplicationContext());
+        }
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(ctxReference.get(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ctxReference.get().startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityDelayRunnable = () -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-        };
+
         launchScreenDelayHandler = new Handler();
+        activityDelayRunnable = new ActivityDelay(this);
         launchScreenDelayHandler.postDelayed(activityDelayRunnable, SPLASH_DISPLAY_LENGTH);
     }
 
