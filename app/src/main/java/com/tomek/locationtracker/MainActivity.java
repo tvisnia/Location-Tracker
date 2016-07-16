8ebaf2ea147b4774feb104d20f526dd1864865e2
Tomek NA <masterworks41@gmail.com>
2016-07-16 19:28:58 +0200

    +small fixes +gitignore fixed

--- app/src/main/java/com/tomek/locationtracker/MainActivity.java
+++ app/src/main/java/com/tomek/locationtracker/MainActivity.java
@@ -1,16 +1,15 @@
 package com.tomek.locationtracker;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.KeyEvent;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.tomek.locationtracker.utils.ApiChecker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,43 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    protected void onResume() {
         checkLocationServices();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
-        finish();
+        super.onResume();
     }
 
     private boolean isLocationEnabled() {
         int locationMode = 0;
         String locationProviders;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (ApiChecker.KitkatOrNewer()) {
             try {
                 locationMode = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
             } catch (Settings.SettingNotFoundException e) {
@@ -78,20 +54,27 @@ public class MainActivity extends AppCompatActivity {
                     .content(R.string.dialog_content)
                     .positiveText(R.string.agree)
                     .negativeText(R.string.disagree)
+                    .keyListener((dialog, keyCode, event) -> {
+                                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                                    dialog.dismiss();
+                                    finish();
+                                    return true;
+                                } else return false;
+                            }
+                    )
                     .callback(new MaterialDialog.ButtonCallback() {
                         @Override
                         public void onPositive(MaterialDialog dialog) {
                             Intent locationSettingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                             startActivity(locationSettingsIntent);
-                            super.onPositive(dialog);
                         }
 
                         @Override
                         public void onNegative(MaterialDialog dialog) {
                             finish();
-                            super.onNegative(dialog);
                         }
                     })
+                    .autoDismiss(false)
                     .build()
                     .show();
         }
