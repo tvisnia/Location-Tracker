a4a5887ca5bd9cde0d2632e86adedc443423ddcf
Tomek NA <masterworks41@gmail.com>
2016-08-06 13:19:45 +0200

    +javadoc fixed in a model LocationData class +empty line removed

--- app/src/main/java/com/tomek/locationtracker/model/LocationData.java
+++ app/src/main/java/com/tomek/locationtracker/model/LocationData.java
@@ -7,22 +7,38 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 /**
- * A model class containing information about given location.
- **/
+ * Location model object
+ *
+ * <P> Contains attributes of received location.
+ *
+ * @author Tomasz Wisniewski
+ */
 public class LocationData {
-    /** The formatter parsing given DateTime object to a String with a DATE_TIME_FORMAT pattern. */
+
+    /**
+     * The formatter parsing given DateTime object to a String with a DATE_TIME_FORMAT pattern.
+     */
     private static final DateTimeFormatter dateAndTime = DateTimeFormat.forPattern(Constants.DATE_TIME_FORMAT);
 
-    /** Geographical longitude and lattitude concatenated as a String object. */
+    /**
+     * Geographical longitude and lattitude concatenated as a String object.
+     */
     public final String coordinates;
-    /** The name of city derived from coordinates (requires internet connection). */
+    /**
+     * The name of city derived from reverse geocoding service.
+     */
     public final String city;
-    /** The date and time of given location. */
+    /**
+     * The date and time of given location.
+     */
     public final DateTime time;
-
 
     /**
      * Constructor.
+     *
+     * @param city        value for {@link #city}
+     * @param coordinates value for {@link #coordinates}
+     * @param dateTime    value for {@link #time}
      */
     public LocationData(String city, String coordinates, DateTime dateTime) {
         this.city = city;
